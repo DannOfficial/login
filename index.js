e9e388ab3b62325673488b5142a7b1032ddc068b
DannTeam <110782241+DannOfficial@users.noreply.github.com>
2024-05-31 13:54:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 const express = require('express')
 const axios = require('axios')
+const session = require('express-session')
+const path = require('path')
 const app = express()
 require('dotenv').config()
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({
+    secret: 'DannTeam',
+    resave: false,
+    saveUninitialized: true
+}))
 
 const port = process.env.PORT
 const APP_ID = process.env.APP_ID
 const APP_SECRET = process.env.APP_SECRET
-const REDIRECT_URI = 'https://api.dannteam.com/callback'
+const REDIRECT_URI = 'http://api.dannteam.com/callback'
 
+// Home route
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/views/login.ejs")
+    res.render('login')
 })
 
+// OAuth redirect route
 app.get('/auth', (req, res) => {
     const authUrl = `https://teman.social/api/oauth2?app_id=${APP_ID}`
     res.redirect(authUrl)
 })
 
+// OAuth callback route
 app.get('/callback', async (req, res) => {
     const authKey = req.query.auth_key
 
@@ -30,10 +41,12 @@ app.get('/callback', async (req, res) => {
         req.session.accessToken = accessToken
         res.redirect('/profile')
     } catch (error) {
-        res.send('Terjadi kesalahan saat mengambil data akses token.')
+        console.error('Error while fetching access token:', error)
+        res.status(500).send('Error while fetching access token.')
     }
 })
 
+// Profile route
 app.get('/profile', async (req, res) => {
     if (!req.session.accessToken) {
         return res.redirect('/')
@@ -44,13 +57,19 @@ app.get('/profile', async (req, res) => {
         const userInfo = response.data
         res.render('profile', { user: userInfo })
     } catch (error) {
-        res.send('Terjadi kesalahan saat mengambil data user informasi.')
+        console.error('Error while fetching user information:', error)
+        res.status(500).send('Error while fetching user information.')
     }
 })
 
+// Logout route
 app.get('/logout', (req, res) => {
-    req.session.destroy()
-    res.redirect('/')
+    req.session.destroy(err => {
+        if (err) {
+            return res.status(500).send('Failed to logout.')
+        }
+        res.redirect('/')
+    })
 })
 
 app.listen(port, () => {
